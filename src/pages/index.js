414807bca422021397221d17be1580ab923feba5
David Eads <davideads@gmail.com>
2019-10-15 22:14:56 -0500

    remove unused imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import Unit from "../components/units"
 import ChiVote from "../components/chivote"
 import Sub from "../components/subform"
-import Footer from "../components/footer"
-
-
 
 import SEO from "../components/seo"
 
