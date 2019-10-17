7c95dc1f588ba283ec471c6bae0fd02a0ddddffa
David Eads <davideads@gmail.com>
2019-10-16 20:52:06 -0500

    that didn't fix the fouc

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,27 +1,27 @@
-// const URL_PREFIX = "electoraid.org"
+const URL_PREFIX = "electoraid.org"
 
-// // Dispatch prefixed pageview
-// exports.onRouteUpdate = ({ location }) => {
-//   if (process.env.NODE_ENV !== `production` || typeof gtag !== `function`) {
-//     return null
-//   }
+// Dispatch prefixed pageview
+exports.onRouteUpdate = ({ location }) => {
+  if (process.env.NODE_ENV !== `production` || typeof gtag !== `function`) {
+    return null
+  }
 
-//   // wrap inside a timeout to make sure react-helmet is done with its changes (https://github.com/gatsbyjs/gatsby/issues/11592)
-//   const sendPageView = () => {
-//     const pagePath = URL_PREFIX + (location
-//       ? location.pathname + location.search + location.hash
-//       : undefined)
-//     window.gtag(`event`, `page_view`, { page_path: pagePath })
-//   }
+  // wrap inside a timeout to make sure react-helmet is done with its changes (https://github.com/gatsbyjs/gatsby/issues/11592)
+  const sendPageView = () => {
+    const pagePath = URL_PREFIX + (location
+      ? location.pathname + location.search + location.hash
+      : undefined)
+    window.gtag(`event`, `page_view`, { page_path: pagePath })
+  }
 
-//   if (`requestAnimationFrame` in window) {
-//     requestAnimationFrame(() => {
-//       requestAnimationFrame(sendPageView)
-//     })
-//   } else {
-//     // simulate 2 rAF calls
-//     setTimeout(sendPageView, 32)
-//   }
+  if (`requestAnimationFrame` in window) {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(sendPageView)
+    })
+  } else {
+    // simulate 2 rAF calls
+    setTimeout(sendPageView, 32)
+  }
 
-//   return null
-// }
+  return null
+}
